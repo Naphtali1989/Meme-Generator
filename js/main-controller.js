63e43bf86e9657d403e6472507df15cb9826560c
Naphtali Rubin <naphtali.r.89@gmail.com>
2020-10-21 11:45:58 +0300

    added a basic render to the gallery

--- js/main-controller.js
+++ js/main-controller.js
@@ -2,4 +2,21 @@
 
 function onInitPage() {
     console.log('Hi Gallery!')
+    renderGallery();
+}
+
+function renderGallery() {
+    const imgs = getImgsToShow();
+    var strHTMLs = imgs.map(img => {
+        return `
+        <img src="${img.url}"
+        class="gallery-item item-${img.id}" 
+        onclick="onOpenEditor('${img.id}')">
+        
+        `
+    });
+    var elGallery = document.querySelector('.main-gallery');
+    console.log(imgs, strHTMLs)
+    elGallery.innerHTML = strHTMLs.join('');
+
 }
--- js/services/gallery-service.js
+++ js/services/gallery-service.js
@@ -1,1 +1,30 @@
 'use strict';
+
+var gKeywords = {};
+var gImgs;
+
+
+function getImgsToShow() {
+    console.log('Gettingg some imgs');
+    gImgs = _createImgs();
+    console.log(gImgs)
+    return gImgs;
+}
+
+
+
+function _createImg(url, keywords) {
+    return {
+        id: makeId(20),
+        url,
+        keywords
+    }
+}
+
+function _createImgs() {
+    var imgs = [];
+    for (let i = 0; i < 18; i++) {
+        imgs.push(_createImg(`img/meme-imgs/${i+1}.jpg`, ['happy']));
+    }
+    return imgs;
+}
